4418aa65c5055e063a5dd4864c2efaa138b943bd
Nurmmuhamad_dev <nsadriddinov99@gmail.com>
2024-09-26 16:55:03 +0500

    index

---
+++ github/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.main {
+  width: 1440px;
+  height: 100px;
+  border: 1px solid black;
+}
+</style>
